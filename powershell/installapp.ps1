5492274a68e7a6aacc0b8cce4b1b6740a15be351
Mario Shimao <mshimao@hotmail.com>
2020-10-24 19:47:36 -0300

    correcao no script de criacao do app

--- powershell/installapp.ps1
+++ powershell/installapp.ps1
@@ -6,20 +6,9 @@ $package = "C:\Handson\handsonapplicationinsights\appgx\DeploymentUnit1.zip"
 $appName = "demoappinsights"
 $physicalPath = "C:\inetpub\wwwroot\demoappinsights"
 $siteName = "Default Web Site"
+$SiteAppPool = "demoappinsights"
 
 New-Item -ItemType Directory -Force -Path $physicalPath
 Expand-Archive -LiteralPath $package -DestinationPath $physicalPath
 
-$appPool = $sm.ApplicationPools[$appName]
-if ($appPool -eq $null) {
-    $appPool = $sm.ApplicationPools.Add($appName)
-}
-
-$site = $sm.Sites[$siteName]
-$app = $site.Applications["/$appName"]
-if ($app -eq $null) {
-    $app = $site.Applications.Add("/$appName", $physicalPath)
-}
-$app.ApplicationPoolName = $appName
- 
-$sm.CommitChanges()
+New-WebApplication -Name $appName -Site $siteName  -PhysicalPath $physicalPath -ApplicationPool $SiteAppPool 
