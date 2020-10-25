351e46e9e8552b5260d6fa80809e1bc9930f2993
Mario Shimao <mshimao@hotmail.com>
2020-10-24 21:02:22 -0300

    start sql browser

--- powershell/settcpipsql.ps1
+++
@@ -1,4 +0,0 @@
-Import-Module SqlServer
-
-CD SQLSERVER:\SQL\localhost\SQLEXPRESS2014
-Set-SqlNetworkConfiguration -Credential $credential -Protocol TCP -Port 1433 -NoServiceRestart -AcceptSelfSignedCertificate
